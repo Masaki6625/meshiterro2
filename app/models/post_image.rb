33719c0b3e7481088f278c87a72d3a65a30d8560
oku <yotubaki8171@gmail.com>
2023-04-29 08:15:25 +0000

    end

---
+++ app/models/post_image.rb
@@ -0,0 +1,17 @@
+class PostImage < ApplicationRecord
+
+  has_one_attached :image
+  belongs_to :user
+
+  validates :shop_name,presence: true
+  validates :image, presence: true
+
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attached(io: File.open(file_path),filename: 'default-image.jpg', content_type: 'image/jpeg')
+    else
+      image
+    end
+  end
+end
